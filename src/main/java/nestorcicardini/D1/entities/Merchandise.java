fd517e5e69d8b9b39bc2df478d9332fafb8c0697
Nestor162 <nestorcicardini@gmail.com>
2023-05-29 16:23:41 +0200

    Creata classe Merchandise

---
+++ src/main/java/nestorcicardini/D1/entities/Merchandise.java
@@ -0,0 +1,18 @@
+package nestorcicardini.D1.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Merchandise extends Product {
+
+	public Merchandise(String name, double price, int calories) {
+		super(name, price, calories);
+	}
+
+}
