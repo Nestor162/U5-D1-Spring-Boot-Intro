b451c3544ffd893ccf22f56511ea98e72bfba83c
Nestor162 <nestorcicardini@gmail.com>
2023-05-29 14:36:07 +0200

    Configurazione iniziale

---
+++ src/test/java/nestorcicardini/D1/D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package nestorcicardini.D1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
